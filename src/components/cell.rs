d80a60ecf9410dbea0bf74d42834506544310ca0
Dan White <dan.mw1@hotmail.co.uk>
2025-09-27 20:24:53 +0100

    Can reveal cell

--- src/components/cell.rs
+++ src/components/cell.rs
@@ -1,10 +1,15 @@
 struct Cell {
     flagged: bool,
+    revealed: bool,
 }
 
 impl Cell {
     pub fn toggle_flag(&mut self) {
         self.flagged = !self.flagged;
+    }
+
+    pub fn reveal(&mut self) {
+        self.revealed = true;
     }
 }
 
@@ -14,7 +19,10 @@ mod tests {
 
     #[test]
     fn can_toggle_flag() {
-        let mut cell = Cell { flagged: false };
+        let mut cell = Cell {
+            flagged: false,
+            revealed: false,
+        };
 
         // Flag is off
         assert!(!cell.flagged);
@@ -26,5 +34,24 @@ mod tests {
         // And off
         cell.toggle_flag();
         assert!(!cell.flagged);
+    }
+
+    #[test]
+    fn can_reveal_cell() {
+        let mut cell = Cell {
+            flagged: false,
+            revealed: false,
+        };
+
+        // Cell is not revealed
+        assert!(!cell.revealed);
+
+        // Can reveal
+        cell.reveal();
+        assert!(cell.revealed);
+
+        // Calling again keeps cell revealed
+        cell.reveal();
+        assert!(cell.revealed);
     }
 }
