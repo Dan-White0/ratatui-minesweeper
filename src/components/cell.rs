6d246f9821b1ef360e37bd29536895ca69ba677c
Dan White <dan.mw1@hotmail.co.uk>
2025-09-27 20:33:57 +0100

    Can mark cell as bomb

--- src/components/cell.rs
+++ src/components/cell.rs
@@ -1,6 +1,8 @@
-struct Cell {
+#[derive(Default)]
+pub struct Cell {
     flagged: bool,
     revealed: bool,
+    is_bomb: bool,
 }
 
 impl Cell {
@@ -11,6 +13,10 @@ impl Cell {
     pub fn reveal(&mut self) {
         self.revealed = true;
     }
+
+    pub fn place_bomb(&mut self) {
+        self.is_bomb = true;
+    }
 }
 
 #[cfg(test)]
@@ -19,10 +25,7 @@ mod tests {
 
     #[test]
     fn can_toggle_flag() {
-        let mut cell = Cell {
-            flagged: false,
-            revealed: false,
-        };
+        let mut cell = Cell::default();
 
         // Flag is off
         assert!(!cell.flagged);
@@ -38,10 +41,7 @@ mod tests {
 
     #[test]
     fn can_reveal_cell() {
-        let mut cell = Cell {
-            flagged: false,
-            revealed: false,
-        };
+        let mut cell = Cell::default();
 
         // Cell is not revealed
         assert!(!cell.revealed);
@@ -53,5 +53,17 @@ mod tests {
         // Calling again keeps cell revealed
         cell.reveal();
         assert!(cell.revealed);
+    }
+
+    #[test]
+    fn can_mark_as_bomb() {
+        let mut cell = Cell::default();
+
+        // Cell is not a bomb
+        assert!(!cell.is_bomb);
+
+        // Can mark as bomb
+        cell.place_bomb();
+        assert!(cell.is_bomb);
     }
 }
