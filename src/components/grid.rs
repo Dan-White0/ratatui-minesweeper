a61c494d5470ea8fc7cc233d497713f5517f8b84
Dan White <dan.mw1@hotmail.co.uk>
2025-09-27 21:48:56 +0100

    Correct bomb insertion, add test for no bombs

--- src/components/grid.rs
+++ src/components/grid.rs
@@ -1,8 +1,9 @@
+use anyhow::{Error, anyhow};
 use rand::seq::index::sample;
 
 use crate::components::Cell;
 
-#[derive(Default)]
+#[derive(Debug, Default)]
 struct Grid {
     rows: Vec<Vec<Cell>>,
     cursor_row: usize,
@@ -10,23 +11,42 @@ struct Grid {
 }
 
 impl Grid {
-    pub fn new(number_of_rows: usize, number_of_columns: usize, number_of_bombs: usize) -> Self {
-        let mut rows = vec![vec![Cell::default(); number_of_columns]; number_of_rows];
+    pub fn new(
+        number_of_rows: usize,
+        number_of_columns: usize,
+        number_of_bombs: usize,
+    ) -> Result<Self, Error> {
+        if number_of_bombs == 0 {
+            return Err(anyhow!("Cannot create a grid with no bombs"));
+        }
 
         let total_cells = number_of_rows * number_of_columns;
+
+        if number_of_bombs > total_cells {
+            return Err(anyhow!(
+                "Attempting to create a grid with more bombs than cells: {}x{} grid ({} cells), but tried to insert {} bombs",
+                number_of_rows,
+                number_of_columns,
+                total_cells,
+                number_of_bombs
+            ));
+        }
+
+        let mut rows = vec![vec![Cell::default(); number_of_columns]; number_of_rows];
+
         let mut rng = rand::rng();
 
         for index in sample(&mut rng, total_cells, number_of_bombs).iter() {
-            let row_index = index / number_of_rows;
+            let row_index = index / number_of_columns;
             let column_index = index % number_of_columns;
 
             rows[row_index][column_index].place_bomb();
         }
 
-        Grid {
+        Ok(Grid {
             rows,
             ..Default::default()
-        }
+        })
     }
 }
 
@@ -40,20 +60,46 @@ mod tests {
     #[test_case(1 , 100; "wide grid")]
     #[test_case(100 , 1; "tall grid")]
     #[test_case(100 , 100; "large grid")]
-    fn can_create_grid(grid_height: usize, grid_width: usize) {
-        let grid = Grid::new(grid_height, grid_width, 0);
+    fn can_create_grid(number_of_rows: usize, number_of_columns: usize) {
+        let grid = Grid::new(number_of_rows, number_of_columns, 1);
 
-        assert_eq!(grid.rows.len(), grid_height);
-        assert_eq!(grid.rows[0].len(), grid_width);
+        assert!(grid.is_ok());
+
+        let grid = grid.unwrap();
+
+        assert_eq!(grid.rows.len(), number_of_rows);
+        assert_eq!(grid.rows[0].len(), number_of_columns);
         assert_eq!(grid.cursor_row, 0);
         assert_eq!(grid.cursor_column, 0);
     }
 
-    #[test_case(0 ; "no bombs")]
+    #[test]
+    fn return_err_if_trying_to_create_grid_with_more_bombs_than_cells() {
+        let error = Grid::new(2, 3, 7).unwrap_err();
+
+        // Check top error or context
+        assert_eq!(
+            format!("{}", error),
+            "Attempting to create a grid with more bombs than cells: 2x3 grid (6 cells), but tried to insert 7 bombs"
+        );
+    }
+
+    #[test]
+    fn return_err_if_trying_to_create_grid_with_no_bombs() {
+        let error = Grid::new(2, 3, 0).unwrap_err();
+
+        // Check top error or context
+        assert_eq!(format!("{}", error), "Cannot create a grid with no bombs");
+    }
+
     #[test_case(1 ; "one bomb")]
     #[test_case(10 ; "many bombs")]
     fn can_create_grid_with_expected_number_of_bombs(expected_number_of_bombs: usize) {
         let grid = Grid::new(5, 5, expected_number_of_bombs);
+
+        assert!(grid.is_ok());
+
+        let grid = grid.unwrap();
 
         let mut number_of_bombs = 0;
 
